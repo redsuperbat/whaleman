3ce621665ec15616ee05b72c12dc2255c42bbf87
Max Netterberg <max.netterberg@gmail.com>
2022-12-18 17:14:51 +0100

    trying out

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"strings"
+	"sync"
 )
 
 const (
@@ -15,13 +17,13 @@ const (
 	COMPOSE_FILE = DATA_DIR + "/compose-file"
 )
 
-func handleComposeChanges(writer http.ResponseWriter, req *http.Request) {
-	fileUrl := os.Getenv("COMPOSE_URL")
-	if fileUrl == "" {
-		log.Fatalln("Please supply the env variable COMPOSE_URL")
-	}
-	response, err := http.Get(fileUrl)
-
+func downloadGithubFile(url string) []byte {
+	ghToken := os.Getenv("GH_PAT")
+	request, err := http.NewRequest("GET", url, nil)
+	request.Header.Add("Authorization", "token "+ghToken)
+	request.Header.Add("Accept", "application/vnd.github.v3.raw")
+	client := http.Client{}
+	response, err := client.Do(request)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -30,7 +32,11 @@ func handleComposeChanges(writer http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	return b
+}
 
+func checkFile(url string) {
+	b := downloadGithubFile(url)
 	b2, err := ioutil.ReadFile(COMPOSE_FILE)
 	if err != nil {
 		log.Fatalln(err)
@@ -47,8 +53,29 @@ func handleComposeChanges(writer http.ResponseWriter, req *http.Request) {
 	exec.Command("docker-compose", "restart", "-f", COMPOSE_FILE)
 }
 
+func getUrls() []string {
+	urls := strings.Split(os.Getenv("GH_COMPOSE_FILES"), ",")
+	if len(urls) == 0 {
+		log.Fatalln("Please supply the env variable GH_COMPOSE_FILES")
+	}
+	return urls
+}
+
 func main() {
-	http.HandleFunc("/handle-changes", handleComposeChanges)
+	http.HandleFunc("/handle-changes", func(w http.ResponseWriter, r *http.Request) {
+		urls := getUrls()
+		var wg sync.WaitGroup
+		wg.Add(len(urls))
+
+		for _, url := range urls {
+			u := url
+			// Run every url request in parallell
+			go func() {
+				checkFile(u)
+				wg.Done()
+			}()
+		}
+	})
 	log.Println("Started server")
 	http.ListenAndServe(":8090", nil)
 }
