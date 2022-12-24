670f2a81de9c303061705369d5cc04314cad00ff
Max Netterberg <max.netterberg@gmail.com>
2022-12-24 14:10:03 +0100

    Fixed some error messages for manifest endpoints

--- manifests/router.go
+++ manifests/router.go
@@ -13,14 +13,20 @@ func createManifestResource(ctx iris.Context) {
 	var Body struct {
 		Url string `json:"url"`
 	}
+
 	if err := ctx.ReadJSON(&Body); err != nil {
 		ctx.StatusCode(400)
-		ctx.JSON(Msg{Message: "Invalid request body"})
+		ctx.JSON(Msg{Message: "Invalid request body: " + err.Error()})
 		return
 	}
 
-	data.WriteManifestResource(Body.Url)
-	ctx.JSON(Msg{Message: "Added resource"})
+	if err := data.WriteManifestResource(Body.Url); err != nil {
+		ctx.StatusCode(500)
+		ctx.JSON(Msg{Message: "Unable to add manifest resource"})
+		ctx.Application().Logger().Error(err)
+		return
+	}
+	ctx.JSON(Msg{Message: "Added resource" + Body.Url})
 }
 
 func RegisterManifests(app *iris.Application) {
